22382a543f139d3958c9536dcce452eba28d9478
jichengwang <602860321@qq.com>
2017-06-30 17:33:01 +0800

    add the setLogger method

--- unilog.go
+++ unilog.go
@@ -1,6 +1,8 @@
 package unilog
 
 import (
+	"fmt"
+	"os"
 	"sync"
 	"time"
 )
@@ -9,6 +11,10 @@ const (
 	ADAPTER_CONSOLE = "console"
 	ADAPTER_FILE    = "file"
 )
+
+type newLoggerFunc func() Logger
+
+var adapters = make(map[string]newLoggerFunc)
 
 type UniLogger struct {
 	lock    sync.Mutex
@@ -29,6 +35,27 @@ type nameLogger struct {
 	name string
 }
 
+func (m *UniLogger) setLogger(adapterName string, configs ...string) error {
+	config := append(configs, "{}")[0]
+	for _, l := range m.outputs {
+		if l.name == adapterName {
+			return fmt.Errorf("logs: duplicate adaptername %q (you have set this logger before)", adapterName)
+		}
+	}
+	newLogger, ok := adapters[adapterName]
+	if !ok {
+		return fmt.Errorf("logs: unknown adaptername %q (forgotten Register?)", adapterName)
+	}
+	lg := newLogger()
+	err := lg.Init(config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "logs.BeeLogger.SetLogger: "+err.Error())
+		return err
+	}
+	m.outputs = append(m.outputs, &nameLogger{name: adapterName, Logger: lg})
+	return nil
+}
+
 func (m *UniLogger) SetLogger(adapterName string, configs ...string) error {
 	m.lock.Lock()
 	defer m.lock.Unlock()
@@ -36,5 +63,23 @@ func (m *UniLogger) SetLogger(adapterName string, configs ...string) error {
 		m.outputs = []*nameLogger{}
 		m.init = true
 	}
+	return nil
+}
+
+func (m *UniLogger) DelLogger(adapterName string) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	outputs := []*nameLogger{}
+	for _, lg := range m.outputs {
+		if lg.name == adapterName {
+			lg.Destroy()
+		} else {
+			outputs = append(outputs, lg)
+		}
+	}
+	if len(outputs) == len(m.outputs) {
+		return fmt.Errorf("logs: unknown adaptername %q (forgotten Register?)", adapterName)
+	}
+	m.outputs = outputs
 	return nil
 }
